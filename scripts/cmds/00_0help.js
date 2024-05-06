046c043a8d5ffc01cfdb44bd77bb24f1eb789128
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-05-06 18:35:45 +0800

    Update 00_0help.js

--- scripts/cmds/00_0help.js
+++ scripts/cmds/00_0help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "⾕ | 𝙔𝘼𝙕𝙆𝙔 𝘼𝙄";
+const doNotDelete = "⾕ | ERICSON AI";
 /** 
 * @author NTKhang
 * @author: do not delete it
